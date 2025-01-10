69685fafe168d1b47a2246f643662450018b1daa
04harsh-vardhan <harshvardhan.s@intimetec.com>
2025-01-10 13:56:13 +0530

    few:w
    1

--- commands.sh
+++ commands.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "starting the script"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 echo "script ended"
