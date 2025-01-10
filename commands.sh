2980ac8651d77b11375599fc0fcfd553736cbdea
04harsh-vardhan <harshvardhan.s@intimetec.com>
2025-01-10 16:08:32 +0530

    changes

--- commands.sh
+++ commands.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "starting the script"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+source ~/.bashrc
+nvm install latest
+nvm install node
 echo "script ended"
